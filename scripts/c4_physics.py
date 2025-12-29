63d8f8a9694eafb7c961f8910bac95f1cd4b09c3
Chethana Dassanayake <128116344+IT22886804@users.noreply.github.com>
2025-12-29 20:46:37 +0530

    fixed typo

--- scripts/c4_physics.py
+++ scripts/c4_physics.py
@@ -18,11 +18,11 @@ class ClockPhysicsEngine:
         # 1. Calculate Theoretical Angles for ALL 720 possible times
         # Hour hand moves 0.5 degrees per minute (360 deg / 720 min)
         theory_h = (self.POSSIBLE_MINUTES * 0.5) % 360
-        # Minute hand moves 6 degrees per minute
+        # Minute hand moves 6 degrees per minute.
         theory_m = (self.POSSIBLE_MINUTES * 6) % 360
         
-        # 2. Test Hypothesis A: Angle1 is Hour, Angle2 is Minute
-        # Error = distance(Angle1, TheoryH) + distance(Angle2, TheoryM)
+        # 2. Test Hypothesis A: Angle1 is Hour, Angle2 is Minute.
+        # Error = distance(Angle1, TheoryH) + distance(Angle2, TheoryM).
         error_a = self.get_angle_diff(angle1, theory_h) + \
                   self.get_angle_diff(angle2, theory_m)
                   
@@ -41,7 +41,7 @@ class ClockPhysicsEngine:
             best_idx = np.argmin(error_b)
             score = min_error_b
             
-        # 5. Convert index (0-719) back to HH:MM
+        # 5. Convert index (0-719) back to HH:MM.
         best_hour = int(best_idx // 60)
         best_minute = int(best_idx % 60)
         if best_hour == 0: best_hour = 12
