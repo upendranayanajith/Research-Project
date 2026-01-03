9d866f71a4c98da064e42fbee3627a6bf12361ef
Heshara1127 <it22207586@my.sliit.lk>
2026-01-03 22:32:21 +0530

    Add initial implementation of ClockLocalizer class with input processing methods

---
+++ scripts/c1_localization.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+from ultralytics import YOLO
+
+class ClockLocalizer:
+    def __init__(self, model_path, use_enhancer=True):
+        pass
+
+    def process_input(self, image):
+        pass
